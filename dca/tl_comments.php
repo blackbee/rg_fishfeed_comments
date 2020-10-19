8fdd432d80ce668ff730deba060d9dfcc3b3e877
Benjamin Geiger <benjamin.geiger@blackbee.eu>
2020-10-19 17:29:55 +0200

    Fix: Entfernen von ungültigen Leerzeilen

--- dca/tl_comments.php
+++ dca/tl_comments.php
@@ -1,7 +1,4 @@
-
 <?php
-
-
 
 $GLOBALS['TL_DCA']['tl_comments']['fields']['sterne'] = [
 
@@ -14,8 +11,6 @@ $GLOBALS['TL_DCA']['tl_comments']['fields']['sterne'] = [
     'eval'                  => ['tl_class'=>'w100'],
     'sql'                   => "varchar(255) NOT NULL default ''"
 ];
-
-
 
 $GLOBALS['TL_DCA']['tl_comments']['fields']['idprodukt'] = [
 	'label'                 => &$GLOBALS['TL_LANG']['tl_comments']['idprodukt'],
@@ -46,4 +41,3 @@ $GLOBALS['TL_DCA']['tl_comments']['fields']['nickname'] = [
 
 
 $GLOBALS['TL_DCA']['tl_comments']['palettes']['default'] = '{author_legend},name,email,website;{comment_legend},sterne,nickname,idprodukt,headline,comment;{reply_legend},addReply;{publish_legend},published';
-
