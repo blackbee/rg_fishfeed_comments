cee4f5274c2d4ca894a5024d6ce0479503e6c853
Benjamin Geiger <benjamin.geiger@blackbee.eu>
2020-10-19 17:46:22 +0200

    Task: Update-Sichere Version von Comments.php erstellen und nicht mehr benötigte Dateien entfernen

---
+++ config/autoload.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2020 Leo Feyer
+ *
+ * @package   zComments
+ * @author    Benjamin Geiger
+ * @license   Commercial
+ * @copyright Benjamin Geiger 2020
+ * @file      config/autoload.php
+ * @version   1.00.00
+ * @date      202010160000
+ */
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+    // Classes
+    'Contao\Comments'                                                       => 'system/modules/zComments/classes/Comments.php',
+));
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+    'com_default.html5'                                                     => 'system/modules/zComments/templates/comments',
+    'ce_comments.html5'                                                     => 'system/modules/zComments/templates/elements',
+    'mod_comment_form.html5'                                                => 'system/modules/zComments/templates/modules',
+    'mod_comments.html5'                                                    => 'system/modules/zComments/templates/modules',
+));
